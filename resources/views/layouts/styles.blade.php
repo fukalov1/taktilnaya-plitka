1f8fd2d3432de9fc2f1cf2a990ec8934f21945ea
Fukalov <andrey.fukalov@payoola.com>
2020-03-03 11:34:34 +0400

    add google and collibry

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -3,3 +3,15 @@
 {{--<link rel="stylesheet" href="{{asset('/libs/fontawesome-free-5.9.0-web/css/all.min.css')}}">--}}
 <link rel="stylesheet" href="{{asset('/css/main.css')}}">
 {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">--}}
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-159434975-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-159434975-1');
+</script>
+
+<script src="//cdn.callibri.ru/callibri.js" type="text/javascript" charset="utf-8"></script>
